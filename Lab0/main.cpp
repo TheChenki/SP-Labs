fdd490658582140b232d48cae7f3c8b0d7dbab96
Denis Keshishyan <nikio.chenki@gmail.com>
2020-02-24 15:16:07 +0200

    Изменил типы данных на правильные

--- Lab0/main.cpp
+++ Lab0/main.cpp
@@ -1,26 +1,29 @@
 #include <stdio.h>
-#include <Windows.h>
+#include <limits.h>
+#include <float.h>
 int main() 
 {
-	printf("\tSHORT\t\tSIZE: %iB\t\tMIN: %i\t\tMAX: %i\n",		sizeof(SHORT), -32768, 32767);
-	printf("\tWORD\t\tSIZE: %iB\t\tMIN: %i\t\t\tMAX: %i\n",		sizeof(WORD), 0, 65535);
-	printf("\tINT\t\tSIZE: %iB\t\tMIN: %i\tMAX: %i\n",			sizeof(INT), -2147483647-1, 2147483647);
-	printf("\tUINT\t\tSIZE: %iB\t\tMIN: %u\t\t\tMAX: %u\n",		sizeof(UINT), 0, 4294967295);
-	printf("\tDWORD\t\tSIZE: %iB\t\tMIN: %u\t\t\tMAX: %u\n",	sizeof(DWORD), 0, 4294967295);
-	printf("\tBYTE\t\tSIZE: %iB\t\tMIN: %i\t\t\tMAX: %i\n",		sizeof(BYTE), 0, 255);
-	printf("\tDOUBLE\t\tSIZE: %iB\t\tMIN: %e\tMAX: %e\n",		sizeof(DOUBLE), 1.7E-308, 1.7E+308);
-	printf("\tFLOAT\t\tSIZE: %iB\t\tMIN: %e\tMAX: %e\n",		sizeof(FLOAT), 3.4E-38, 3.4E+38);
-	printf("\tBOOL\t\tSIZE: %iB\t\tMIN: %i\t\t\tMAX: %i\n",		sizeof(BOOL), FALSE, TRUE);
-	printf("\tBOOLEAN\t\tSIZE: %iB\t\tMIN: %i\t\t\tMAX: %i\n",	sizeof(BOOLEAN), FALSE, TRUE);
-	printf("\tCHAR\t\tSIZE: %iB\t\tMIN: %i\t\tMAX: %i\n",		sizeof(CHAR), -128, 127);
-	printf("\tWCHAR\t\tSIZE: %iB\t\tMIN: %i\t\t\tMAX: %i\n",	sizeof(WCHAR), 0, 65535);
-	printf("\tTCHAR\t\tSIZE: %iB\t\tMIN: %i\t\t\tMAX: %i\n",	sizeof(TCHAR), 0, 65535);
-	printf("\tLPSTR\t\tSIZE: %iB\t\tMIN: %u\t\t\tMAX: %u\n",	sizeof(LPSTR), 0, 4294967295);
-	printf("\tLPCSTR\t\tSIZE: %iB\t\tMIN: %u\t\t\tMAX: %u\n",	sizeof(LPCSTR), 0, 4294967295);
-	printf("\tLPWSTR\t\tSIZE: %iB\t\tMIN: %u\t\t\tMAX: %u\n",	sizeof(LPWSTR), 0, 4294967295);
-	printf("\tLPCWSTR\t\tSIZE: %iB\t\tMIN: %u\t\t\tMAX: %u\n",	sizeof(LPCWSTR), 0, 4294967295);
-	printf("\tLPTSTR\t\tSIZE: %iB\t\tMIN: %u\t\t\tMAX: %u\n",	sizeof(LPTSTR), 0, 4294967295);
-	printf("\tLPCTSTR\t\tSIZE: %iB\t\tMIN: %u\t\t\tMAX: %u\n",	sizeof(LPCTSTR), 0, 4294967295);
-	system("pause");
+	printf("Char:\t\tmin: %i\tmax: %i\tsize: %iB\n",CHAR_MIN,CHAR_MAX,sizeof(char));
+	printf("Signed Char:\tmin: %i\tmax: %i\tsize: %iB\n", SCHAR_MIN, SCHAR_MAX, sizeof(signed char));
+	printf("Unsigned Char:\tmin: %i\t\tmax: %i\tsize: %iB\n\n", 0, UCHAR_MAX, sizeof(unsigned char));
+
+	printf("Bool:\t\tmin: %i\t\tmax: %i\t\tsize: %iB\n\n", false, true, sizeof(bool));
+
+	printf("Short:\t\tmin: %i\tmax: %i\tsize: %iB\n", SHRT_MIN, SHRT_MAX, sizeof(short));
+	printf("Unsigned Short:\tmin: %i\t\tmax: %i\tsize: %iB\n\n", 0, USHRT_MAX, sizeof(unsigned short));
+
+	printf("Int:\t\tmin: %i\tmax: %i\t\tsize: %iB\n", INT_MIN, INT_MAX, sizeof(int));
+	printf("Unsigned Int:\tmin: %i\t\t\tmax: %u\t\tsize: %iB\n\n", 0, UINT_MAX, sizeof(unsigned int));
+
+	printf("Long:\t\tmin: %li\tmax: %li\t\tsize: %iB\n", LONG_MIN, LONG_MAX, sizeof(long));
+	printf("Unsigned Long:\tmin: %lu\t\t\tmax: %lu\t\tsize: %iB\n\n", 0, ULONG_MAX, sizeof(unsigned long));
+
+	printf("Float:\t\tmin: %e\tmax: %e\tsize: %iB\n", FLT_MIN, FLT_MAX, sizeof(float));
+	printf("Double:\t\tmin: %e\tmax: %e\tsize: %iB\n", DBL_MIN, DBL_MAX, sizeof(double));
+	printf("Long Double:\tmin: %le\tmax: %le\tsize: %iB\n\n", LDBL_MIN, LDBL_MAX, sizeof(long double));
+
+	printf("Long Long:\t\tmin: %lli\tmax: %lli\tsize: %iB\n", LLONG_MIN, LLONG_MAX, sizeof(long long));
+	printf("Unsigned Long Long:\tmin: %i\t\t\t\tmax: %llu\tsize: %iB\n\n", 0, ULLONG_MAX, sizeof(unsigned long long));
+
 	return 0;
 }
